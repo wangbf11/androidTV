f7b02c90641db4d13e7da30ea87b0ce164b2e054
wangbf11 <584623818@qq.com>
2020-04-01 21:52:25 +0800

    主页面公告栏目

--- xueliang/app/src/main/java/com/example/xueliang/activity/LoginActivity.java
+++ xueliang/app/src/main/java/com/example/xueliang/activity/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.xueliang.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.xueliang.R;
 
@@ -10,9 +12,22 @@ import com.example.xueliang.R;
  */
 public class LoginActivity extends Activity {
 
+    private ImageView mQrCode;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mQrCode = findViewById(R.id.iv_qrcode);
+        initView();
+    }
+
+    private void initView() {
+        mQrCode.setOnClickListener(v->{
+            Intent intent = new Intent();
+            intent.setClass(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
