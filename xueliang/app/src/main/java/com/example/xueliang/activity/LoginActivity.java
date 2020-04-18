a0aceb9edc30b0316a167264aeb1aa94203071b0
王宝丰  <584623818@qq.com>
2020-04-18 11:45:21 +0800

    更具后端返回字符串 生产登录二维码

--- xueliang/app/src/main/java/com/example/xueliang/activity/LoginActivity.java
+++ xueliang/app/src/main/java/com/example/xueliang/activity/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.example.xueliang.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.xueliang.R;
 import com.example.xueliang.base.BasePresenter;
+import com.example.xueliang.utils.AppUtils;
+import com.example.xueliang.utils.QRCodeUtil;
 
 /*
  * 登录页面
@@ -30,16 +34,20 @@ public class LoginActivity extends BaseMvpActivity {
     @Override
     public void initView() {
         mQrCode = findViewById(R.id.iv_qrcode);
+        Bitmap cdd = QRCodeUtil.syncEncodeQRCode("syncEncodeQRCodesyncEncodeQRCodesyncEncodeQRCodesyncEncodeQRCodesyncEncodeQRCode", AppUtils.dip2px(158));
+        Glide.with(this)
+                .load(cdd)
+                .into(mQrCode);
+
+    }
+
+    @Override
+    public void initListener() {
         mQrCode.setOnClickListener(v->{
             Intent intent = new Intent();
             intent.setClass(LoginActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
         });
-    }
-
-    @Override
-    public void initListener() {
-
     }
 }
