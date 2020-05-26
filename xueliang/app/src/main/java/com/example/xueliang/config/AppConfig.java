de67256e533fdb5036437e708d9a4c27dd684eec
王宝丰  <584623818@qq.com>
2020-05-26 14:31:46 +0800

    版本1。0。20切换到正式环境

--- xueliang/app/src/main/java/com/example/xueliang/config/AppConfig.java
+++ xueliang/app/src/main/java/com/example/xueliang/config/AppConfig.java
@@ -7,7 +7,8 @@ package com.example.xueliang.config;
  */
 public class AppConfig {
     //网络请求host
-    public static String baseUrl = "http://app8848.com/";
+//    public static String baseUrl = "http://app8848.com/";
+    public static String baseUrl = "http://117.139.72.126:8092/";
     /*日志文件总开关 目前默认 debug 才开启 */
     public static Boolean LOG_SWITCH = true;
 }
